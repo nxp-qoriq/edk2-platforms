46787a5828f96bd2ddd1e4cab688bb035a8063c1
Meenakshi Aggarwal <meenakshi.aggarwal@nxp.com>
2018-01-30 01:37:07 +0530

    Silicon/NXP : Add support of IfcLib
    Add support of IfcLib, it will be used to perform
    any operation on IFC controller.
    
    Contributed-under: TianoCore Contribution Agreement 1.1
    Signed-off-by: Meenakshi Aggarwal <meenakshi.aggarwal@nxp.com>

---
+++ Silicon/NXP/Include/Library/IfcLib.h
@@ -0,0 +1,23 @@
+/** @IfcLib.h
+
+  Copyright 2018 NXP
+
+  This program and the accompanying materials
+  are licensed and made available under the terms and conditions of the BSD License
+  which accompanies this distribution.  The full text of the license may be found at
+  http://opensource.org/licenses/bsd-license.php
+
+  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+
+**/
+
+#ifndef __IFC_LIB_H__
+#define __IFC_LIB_H__
+
+VOID
+IfcInit (
+  VOID
+  );
+
+#endif //__IFC_LIB_H__
