7bf3017c19dbabbd69c20cf26f670aa3071afa3a
Meenakshi Aggarwal <meenakshi.aggarwal@nxp.com>
2018-01-30 01:45:08 +0530

    LS1043/FpgaLib : Add support for FpgaLib.
    FpgaLib export FPGA_READ and FPGA_WRITE function and
    provide a function to print Board personality.
    
    Contributed-under: TianoCore Contribution Agreement 1.1
    Signed-off-by: Meenakshi Aggarwal <meenakshi.aggarwal@nxp.com>

---
+++ Platform/NXP/LS1043aRdbPkg/Include/Library/FpgaLib.h
@@ -0,0 +1,79 @@
+/** FpgaLib.h
+*  Header defining the LS1043a Fpga specific constants (Base addresses, sizes, flags)
+*
+*  Copyright 2017 NXP
+*
+*  This program and the accompanying materials
+*  are licensed and made available under the terms and conditions of the BSD License
+*  which accompanies this distribution.  The full text of the license may be found at
+*  http://opensource.org/licenses/bsd-license.php
+*
+*  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+*
+**/
+
+#ifndef __LS1043A_FPGA_H__
+#define __LS1043A_FPGA_H__
+
+/*
+ * FPGA register set of LS1043ARDB board-specific.
+ */
+typedef struct {
+  UINT8  FpgaVersionMajor; /* 0x0 - FPGA Major Revision Register */
+  UINT8  FpgaVersionMinor; /* 0x1 - FPGA Minor Revision Register */
+  UINT8  PcbaVersion;      /* 0x2 - PCBA Revision Register */
+  UINT8  SystemReset;      /* 0x3 - system reset register */
+  UINT8  SoftMuxOn;        /* 0x4 - Switch Control Enable Register */
+  UINT8  RcwSource1;       /* 0x5 - Reset config word 1 */
+  UINT8  RcwSource2;       /* 0x6 - Reset config word 1 */
+  UINT8  Vbank;            /* 0x7 - Flash bank selection Control */
+  UINT8  SysclkSelect;     /* 0x8 - System clock selection Control */
+  UINT8  UartSel;          /* 0x9 - Uart selection Control */
+  UINT8  Sd1RefClkSel;     /* 0xA - Serdes1 reference clock selection Control */
+  UINT8  TdmClkMuxSel;     /* 0xB - TDM Clock Mux selection Control */
+  UINT8  SdhcSpiCsSel;     /* 0xC - SDHC/SPI Chip select selection Control */
+  UINT8  StatusLed;        /* 0xD - Status Led */
+  UINT8  GlobalReset;      /* 0xE - Global reset */
+} FPGA_REG_SET;
+
+UINT8
+FpgaRead (
+  UINTN  Reg
+  );
+
+VOID
+FpgaWrite (
+  UINTN  Reg,
+  UINT8  Value
+  );
+
+VOID
+FpgaRevBit (
+  UINT8  *Value
+  );
+
+VOID
+FpgaInit (
+  VOID
+  );
+
+VOID
+PrintBoardPersonality (
+  VOID
+  );
+
+#define FPGA_BASE_PHYS          0x7fb00000
+
+#define SRC_VBANK               0x25
+#define SRC_NAND                0x106
+#define SRC_QSPI                0x44
+#define SRC_SD                  0x40
+
+#define SERDES_FREQ1            "100.00 MHz"
+#define SERDES_FREQ2            "156.25 MHz"
+
+#define FPGA_READ(Reg)          FpgaRead (OFFSET_OF (FPGA_REG_SET, Reg))
+#define FPGA_WRITE(Reg, Value)  FpgaWrite (OFFSET_OF (FPGA_REG_SET, Reg), Value)
+
+#endif
