980fa8d44d370aee3282ec5f8ebd09894aacdc83
Pankaj Bansal <pankaj.bansal@nxp.com>
2018-03-20 12:06:44 +0530

    Remove unncessary code
    Signed-off-by: Pankaj Bansal <pankaj.bansal@nxp.com>

--- Silicon/NXP/Drivers/SpiBusDxe/SpiBusDxe.c
+++ Silicon/NXP/Drivers/SpiBusDxe/SpiBusDxe.c
@@ -36,8 +36,6 @@
 //
 // Global Variables
 //
-extern EFI_COMPONENT_NAME_PROTOCOL    gSpiBusComponentName;
-extern EFI_COMPONENT_NAME2_PROTOCOL   gSpiBusComponentName2;
 extern EFI_DRIVER_BINDING_PROTOCOL    gSpiBusDriverBinding;
 
 //
@@ -95,169 +93,6 @@ STATIC LIST_ENTRY    mSpiBusList = INITIALIZE_LIST_HEAD_VARIABLE (mSpiBusList);
 
 // Link list of SPI Devices that are runtime
 STATIC LIST_ENTRY    mSpiDeviceList = INITIALIZE_LIST_HEAD_VARIABLE (mSpiDeviceList);
-
-//
-// Driver name table
-//
-GLOBAL_REMOVE_IF_UNREFERENCED EFI_UNICODE_STRING_TABLE mSpiBusDriverNameTable[] = {
-  { "eng;en", (CHAR16 *) L"SPI Bus Driver" },
-  { NULL , NULL }
-};
-
-//
-// EFI Component Name Protocol
-//
-GLOBAL_REMOVE_IF_UNREFERENCED EFI_COMPONENT_NAME_PROTOCOL  gSpiBusComponentName = {
-  (EFI_COMPONENT_NAME_GET_DRIVER_NAME) SpiBusComponentNameGetDriverName,
-  (EFI_COMPONENT_NAME_GET_CONTROLLER_NAME) SpiBusComponentNameGetControllerName,
-  "eng"
-};
-
-//
-// EFI Component Name 2 Protocol
-//
-GLOBAL_REMOVE_IF_UNREFERENCED EFI_COMPONENT_NAME2_PROTOCOL gSpiBusComponentName2 = {
-  SpiBusComponentNameGetDriverName,
-  SpiBusComponentNameGetControllerName,
-  "en"
-};
-
-/**
-  Retrieves a Unicode string that is the user readable name of the driver.
-
-  This function retrieves the user readable name of a driver in the form of a
-  Unicode string. If the driver specified by This has a user readable name in
-  the language specified by Language, then a pointer to the driver name is
-  returned in DriverName, and EFI_SUCCESS is returned. If the driver specified
-  by This does not support the language specified by Language,
-  then EFI_UNSUPPORTED is returned.
-
-  @param  This[in]              A pointer to the EFI_COMPONENT_NAME2_PROTOCOL or
-                                EFI_COMPONENT_NAME_PROTOCOL instance.
-
-  @param  Language[in]          A pointer to a Null-terminated ASCII string
-                                array indicating the language. This is the
-                                language of the driver name that the caller is
-                                requesting, and it must match one of the
-                                languages specified in SupportedLanguages. The
-                                number of languages supported by a driver is up
-                                to the driver writer. Language is specified
-                                in RFC 4646 or ISO 639-2 language code format.
-
-  @param  DriverName[out]       A pointer to the Unicode string to return.
-                                This Unicode string is the name of the
-                                driver specified by This in the language
-                                specified by Language.
-
-  @retval EFI_SUCCESS           The Unicode string for the Driver specified by
-                                This and the language specified by Language was
-                                returned in DriverName.
-
-  @retval EFI_INVALID_PARAMETER Language is NULL.
-
-  @retval EFI_INVALID_PARAMETER DriverName is NULL.
-
-  @retval EFI_UNSUPPORTED       The driver specified by This does not support
-                                the language specified by Language.
-
-**/
-EFI_STATUS
-EFIAPI
-SpiBusComponentNameGetDriverName (
-  IN  EFI_COMPONENT_NAME2_PROTOCOL  *This,
-  IN  CHAR8                        *Language,
-  OUT CHAR16                       **DriverName
-  )
-{
-  return LookupUnicodeString2 (
-           Language,
-           This->SupportedLanguages,
-           mSpiBusDriverNameTable,
-           DriverName,
-           (BOOLEAN)(This != &gSpiBusComponentName2)
-           );
-}
-
-/**
-  Retrieves a Unicode string that is the user readable name of the controller
-  that is being managed by a driver.
-
-  This function retrieves the user readable name of the controller specified by
-  ControllerHandle and ChildHandle in the form of a Unicode string. If the
-  driver specified by This has a user readable name in the language specified by
-  Language, then a pointer to the controller name is returned in ControllerName,
-  and EFI_SUCCESS is returned.  If the driver specified by This is not currently
-  managing the controller specified by ControllerHandle and ChildHandle,
-  then EFI_UNSUPPORTED is returned.  If the driver specified by This does not
-  support the language specified by Language, then EFI_UNSUPPORTED is returned.
-
-  @param  This[in]              A pointer to the EFI_COMPONENT_NAME2_PROTOCOL or
-                                EFI_COMPONENT_NAME_PROTOCOL instance.
-
-  @param  ControllerHandle[in]  The handle of a controller that the driver
-                                specified by This is managing.  This handle
-                                specifies the controller whose name is to be
-                                returned.
-
-  @param  ChildHandle[in]       The handle of the child controller to retrieve
-                                the name of.  This is an optional parameter that
-                                may be NULL.  It will be NULL for device
-                                drivers.  It will also be NULL for a bus drivers
-                                that wish to retrieve the name of the bus
-                                controller.  It will not be NULL for a bus
-                                driver that wishes to retrieve the name of a
-                                child controller.
-
-  @param  Language[in]          A pointer to a Null-terminated ASCII string
-                                array indicating the language.  This is the
-                                language of the driver name that the caller is
-                                requesting, and it must match one of the
-                                languages specified in SupportedLanguages. The
-                                number of languages supported by a driver is up
-                                to the driver writer. Language is specified in
-                                RFC 4646 or ISO 639-2 language code format.
-
-  @param  ControllerName[out]   A pointer to the Unicode string to return.
-                                This Unicode string is the name of the
-                                controller specified by ControllerHandle and
-                                ChildHandle in the language specified by
-                                Language from the point of view of the driver
-                                specified by This.
-
-  @retval EFI_SUCCESS           The Unicode string for the user readable name in
-                                the language specified by Language for the
-                                driver specified by This was returned in
-                                DriverName.
-
-  @retval EFI_INVALID_PARAMETER ControllerHandle is NULL.
-
-  @retval EFI_INVALID_PARAMETER ChildHandle is not NULL and it is not a valid
-                                EFI_HANDLE.
-
-  @retval EFI_INVALID_PARAMETER Language is NULL.
-
-  @retval EFI_INVALID_PARAMETER ControllerName is NULL.
-
-  @retval EFI_UNSUPPORTED       The driver specified by This is not currently
-                                managing the controller specified by
-                                ControllerHandle and ChildHandle.
-
-  @retval EFI_UNSUPPORTED       The driver specified by This does not support
-                                the language specified by Language.
-
-**/
-EFI_STATUS
-EFIAPI
-SpiBusComponentNameGetControllerName (
-  IN  EFI_COMPONENT_NAME2_PROTOCOL                     *This,
-  IN  EFI_HANDLE                                      ControllerHandle,
-  IN  EFI_HANDLE                                      ChildHandle        OPTIONAL,
-  IN  CHAR8                                           *Language,
-  OUT CHAR16                                          **ControllerName
-  )
-{
-  return EFI_UNSUPPORTED;
-}
 
 /**
   Check if the Spi Host controller's device path exists in Spi Bus configuration
@@ -1413,13 +1248,11 @@ InitializeSpiBus(
   //
   // Install driver model protocol(s).
   //
-  Status = EfiLibInstallDriverBindingComponentName2 (
+  Status = EfiLibInstallDriverBinding (
              ImageHandle,
              SystemTable,
              &gSpiBusDriverBinding,
-             NULL,
-             &gSpiBusComponentName,
-             &gSpiBusComponentName2
+             NULL
              );
   ASSERT_EFI_ERROR (Status);
 
@@ -1461,8 +1294,6 @@ SpiBusUnload (
   EFI_HANDLE                        *DeviceHandleBuffer;
   UINTN                             DeviceHandleCount;
   UINTN                             Index;
-  EFI_COMPONENT_NAME_PROTOCOL       *ComponentName;
-  EFI_COMPONENT_NAME2_PROTOCOL      *ComponentName2;
 
   //
   // Get the list of all SPI Controller handles in the handle database.
@@ -1504,41 +1335,6 @@ SpiBusUnload (
                   NULL
                   );
   ASSERT_EFI_ERROR (Status);
-
-  //
-  // Note we have to one by one uninstall the following protocols.
-  // It's because some of them are optionally installed based on
-  // the following PCD settings.
-  //   gEfiMdePkgTokenSpaceGuid.PcdDriverDiagnosticsDisable
-  //   gEfiMdePkgTokenSpaceGuid.PcdComponentNameDisable
-  //   gEfiMdePkgTokenSpaceGuid.PcdDriverDiagnostics2Disable
-  //   gEfiMdePkgTokenSpaceGuid.PcdComponentName2Disable
-  //
-  Status = gBS->HandleProtocol (
-                  gSpiBusDriverBinding.DriverBindingHandle,
-                  &gEfiComponentNameProtocolGuid,
-                  (VOID **) &ComponentName
-                  );
-  if (!EFI_ERROR (Status)) {
-    gBS->UninstallProtocolInterface (
-           gSpiBusDriverBinding.DriverBindingHandle,
-           &gEfiComponentNameProtocolGuid,
-           ComponentName
-           );
-  }
-
-  Status = gBS->HandleProtocol (
-                  gSpiBusDriverBinding.DriverBindingHandle,
-                  &gEfiComponentName2ProtocolGuid,
-                  (VOID **) &ComponentName2
-                  );
-  if (!EFI_ERROR (Status)) {
-    gBS->UninstallProtocolInterface (
-           gSpiBusDriverBinding.DriverBindingHandle,
-           &gEfiComponentName2ProtocolGuid,
-           ComponentName2
-           );
-  }
 
   Status = EFI_SUCCESS;
 
