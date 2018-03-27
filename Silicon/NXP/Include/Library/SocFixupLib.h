a981b88821b069da2e49f2472fff1796a6b97ad7
Pankaj Bansal <pankaj.bansal@nxp.com>
2018-03-27 14:36:31 +0530

    NXP/SocFixupLib: Introduce a library which will do soc related fixups in device tree
    We may need to fix the device tree before OS is able to use it.
    Therefore, introduce a library which will do SOC related fixups in
    device tree.
    
    Contributed-under: TianoCore Contribution Agreement 1.1
    Signed-off-by: Pankaj Bansal <pankaj.bansal@nxp.com>

---
+++ Silicon/NXP/Include/Library/SocFixupLib.h
@@ -0,0 +1,33 @@
+/** @file
+*
+*  Copyright 2018 NXP
+*
+*  This program and the accompanying materials
+*  are licensed and made available under the terms and conditions of the BSD License
+*  which accompanies this distribution.  The full text of the license may be found at
+*  http://opensource.org/licenses/bsd-license.php
+*
+*  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+*
+**/
+
+#ifndef SOC_FIXUP_LIB_H_
+#define SOC_FIXUP_LIB_H_
+
+#include <Uefi.h>
+
+/**
+  Fixup the device tree based on running SOC's properties.
+
+  @param[in]  Dtb   The device tree to fixup.
+
+  @return EFI_SUCCESS       Successfully fix up the device tree
+  @return EFI_DEVICE_ERROR  Could not fixup the device tree
+**/
+EFI_STATUS
+FdtSocFixup (
+  IN  VOID  *Dtb
+  );
+
+#endif // SOC_FIXUP_LIB_H_
