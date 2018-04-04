3023c88d5db8d94b6bc7f4bd681a91a91deeb314
Wasim Khan <wasim.khan@nxp.com>
2018-04-05 01:19:51 +0530

    Silicon/NXP: Add Dpaa2EthernetMacLib Library Support
    Dpaa2EthernetMacLib Library contains code to initialized
    a discovered DPMAC (Data Path MAC object).
    
    Contributed-under: TianoCore Contribution Agreement 1.1
    Signed-off-by: Wasim Khan <wasim.khan@nxp.com>

---
+++ Silicon/NXP/Include/Library/Dpaa2EthernetMacLib.h
@@ -0,0 +1,115 @@
+/** Dpaa2EthernetMacLib.h
+  DPAA2 Ethernet MAC library interface
+
+  Copyright 2017 NXP
+
+  This program and the accompanying materials
+  are licensed and made available under the terms and conditions of the BSD License
+  which accompanies this distribution. The full text of the license may be found at
+  http://opensource.org/licenses/bsd-license.php
+
+  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+
+**/
+
+#ifndef __DPAA2_ETHERTNET_MAC_LIB_H__
+#define __DPAA2_ETHERTNET_MAC_LIB_H__
+
+#include <Library/BaseLib.h>
+#include <Library/Dpaa2EthernetPhyLib.h>
+#include <Uefi.h>
+
+#define MAC_MULTICAST_ADDRESS_BIT   0x1
+
+/**
+ * DPAA2 WRIOP DPMAC IDs
+ *
+ * NOTE: Valid DPMAC IDs must start at 1, since that is what the dpmac_create()
+ * MC command expects.
+ */
+typedef enum _WRIOP_DPMAC_ID {
+  INVALID_WRIOP_DPMAC_ID = 0x0,
+  WRIOP_DPMAC1 = 0x1,
+  WRIOP_DPMAC2,
+  WRIOP_DPMAC3,
+  WRIOP_DPMAC4,
+  WRIOP_DPMAC5,
+  WRIOP_DPMAC6,
+  WRIOP_DPMAC7,
+  WRIOP_DPMAC8,
+  WRIOP_DPMAC9,
+  WRIOP_DPMAC10,
+  WRIOP_DPMAC11,
+  WRIOP_DPMAC12,
+  WRIOP_DPMAC13,
+  WRIOP_DPMAC14,
+  WRIOP_DPMAC15,
+  WRIOP_DPMAC16,
+  WRIOP_DPMAC17,
+  WRIOP_DPMAC18,
+  WRIOP_DPMAC19,
+  WRIOP_DPMAC20,
+  WRIOP_DPMAC21,
+  WRIOP_DPMAC22,
+  WRIOP_DPMAC23,
+  WRIOP_DPMAC24,
+
+  /*
+   * New values must added above this entry
+   */
+  NUM_WRIOP_DPMACS,
+} WRIOP_DPMAC_ID;
+
+/**
+ * State information kept for a WRIOP DPMAC
+ */
+typedef struct _WRIOP_DPMAC {
+  /**
+   * Signature for run-time type checking
+   */
+  UINT32 Signature;
+# define WRIOP_DPMAC_SIGNATURE SIGNATURE_32('D', 'P', 'M', 'A')
+
+  /**
+   * Dpmac ID
+   */
+  WRIOP_DPMAC_ID Id;
+
+  /**
+   * Flag indicating if this Dpmac is enabled (corresponding bit in the
+   * CCSR DEVDISR2 register is off)
+   */
+  BOOLEAN Enabled;
+
+  /**
+   * Node in a linked list of enabled DPMACs
+   */
+  LIST_ENTRY ListNode;
+
+  /**
+   * Ethernet PHY associated with this DPMAC
+   */
+  DPAA2_PHY Phy;
+
+} WRIOP_DPMAC;
+
+VOID
+WriopDpmacInit (
+  WRIOP_DPMAC_ID     DpmacId,
+  PHY_INTERFACE_TYPE PhyInterfaceType,
+  DPAA2_PHY_MDIO_BUS *MdioBus,
+  UINT8              PhyAddress,
+  PHY_MEDIA_TYPE     PhyMediaType,
+  LIST_ENTRY         *EnabledDmpacsList
+  );
+
+CONST
+CHAR8 *
+PhyInterfaceTypeToString (
+  PHY_INTERFACE_TYPE PhyInterfaceType
+  );
+
+extern const CHAR8 *const gWriopDpmacStrings[];
+
+#endif /* __DPAA2_ETHERTNET_MAC_LIB_H__ */
---
+++ Silicon/NXP/Library/Dpaa2EthernetMacLib/WriopDpmac.c
@@ -0,0 +1,166 @@
+/** WriopDpmac.c
+  DPAA2 WRIOP MAC services implementation
+
+  Copyright 2017 NXP
+
+  This program and the accompanying materials
+  are licensed and made available under the terms and conditions of the BSD License
+  which accompanies this distribution. The full text of the license may be found at
+  http://opensource.org/licenses/bsd-license.php
+
+  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+
+**/
+
+#include <Bitops.h>
+#include <Library/DpaaDebugLib.h>
+#include <Library/Dpaa2EthernetMacLib.h>
+#include <Library/IoLib.h>
+#include <Library/PcdLib.h>
+
+const CHAR8 *const gWriopDpmacStrings[] = {
+  [WRIOP_DPMAC1] = "DPMAC1",
+  [WRIOP_DPMAC2] = "DPMAC2",
+  [WRIOP_DPMAC3] = "DPMAC3",
+  [WRIOP_DPMAC4] = "DPMAC4",
+  [WRIOP_DPMAC5] = "DPMAC5",
+  [WRIOP_DPMAC6] = "DPMAC6",
+  [WRIOP_DPMAC7] = "DPMAC7",
+  [WRIOP_DPMAC8] = "DPMAC8",
+  [WRIOP_DPMAC9] = "DPMAC9",
+  [WRIOP_DPMAC10] = "DPMAC10",
+  [WRIOP_DPMAC11] = "DPMAC11",
+  [WRIOP_DPMAC12] = "DPMAC12",
+  [WRIOP_DPMAC13] = "DPMAC13",
+  [WRIOP_DPMAC14] = "DPMAC14",
+  [WRIOP_DPMAC15] = "DPMAC15",
+  [WRIOP_DPMAC16] = "DPMAC16",
+  [WRIOP_DPMAC17] = "DPMAC17",
+  [WRIOP_DPMAC18] = "DPMAC18",
+  [WRIOP_DPMAC19] = "DPMAC19",
+  [WRIOP_DPMAC20] = "DPMAC20",
+  [WRIOP_DPMAC21] = "DPMAC21",
+  [WRIOP_DPMAC22] = "DPMAC22",
+  [WRIOP_DPMAC23] = "DPMAC23",
+  [WRIOP_DPMAC24] = "DPMAC24",
+};
+
+STATIC const CHAR8 *const gPhyInterfaceTypeStrings[] = {
+  [PHY_INTERFACE_XGMII] = "xgmii",
+  [PHY_INTERFACE_SGMII] = "sgmii",
+  [PHY_INTERFACE_QSGMII] = "qsgmii",
+};
+
+#define WRIOP_DPMAC_INITIALIZER(_DpmacId) \
+        [_DpmacId] = {                                                  \
+          .Signature = WRIOP_DPMAC_SIGNATURE,                           \
+          .Id = _DpmacId,                                               \
+          .Enabled = FALSE,                                             \
+          .Phy = {                                                      \
+            .Signature = DPAA2_PHY_SIGNATURE,                           \
+            .PhyInterfaceType = PHY_INTERFACE_NONE,                     \
+          },                                                            \
+        }
+
+STATIC WRIOP_DPMAC gWriopDpmacs[] = {
+  WRIOP_DPMAC_INITIALIZER (WRIOP_DPMAC1),
+  WRIOP_DPMAC_INITIALIZER (WRIOP_DPMAC2),
+  WRIOP_DPMAC_INITIALIZER (WRIOP_DPMAC3),
+  WRIOP_DPMAC_INITIALIZER (WRIOP_DPMAC4),
+  WRIOP_DPMAC_INITIALIZER (WRIOP_DPMAC5),
+  WRIOP_DPMAC_INITIALIZER (WRIOP_DPMAC6),
+  WRIOP_DPMAC_INITIALIZER (WRIOP_DPMAC7),
+  WRIOP_DPMAC_INITIALIZER (WRIOP_DPMAC8),
+  WRIOP_DPMAC_INITIALIZER (WRIOP_DPMAC9),
+  WRIOP_DPMAC_INITIALIZER (WRIOP_DPMAC10),
+  WRIOP_DPMAC_INITIALIZER (WRIOP_DPMAC11),
+  WRIOP_DPMAC_INITIALIZER (WRIOP_DPMAC12),
+  WRIOP_DPMAC_INITIALIZER (WRIOP_DPMAC13),
+  WRIOP_DPMAC_INITIALIZER (WRIOP_DPMAC14),
+  WRIOP_DPMAC_INITIALIZER (WRIOP_DPMAC15),
+  WRIOP_DPMAC_INITIALIZER (WRIOP_DPMAC16),
+  WRIOP_DPMAC_INITIALIZER (WRIOP_DPMAC17),
+  WRIOP_DPMAC_INITIALIZER (WRIOP_DPMAC18),
+  WRIOP_DPMAC_INITIALIZER (WRIOP_DPMAC19),
+  WRIOP_DPMAC_INITIALIZER (WRIOP_DPMAC20),
+  WRIOP_DPMAC_INITIALIZER (WRIOP_DPMAC21),
+  WRIOP_DPMAC_INITIALIZER (WRIOP_DPMAC22),
+  WRIOP_DPMAC_INITIALIZER (WRIOP_DPMAC23),
+  WRIOP_DPMAC_INITIALIZER (WRIOP_DPMAC24),
+};
+
+STATIC
+BOOLEAN
+IsDpmacEnabled (
+  WRIOP_DPMAC_ID DpmacId
+  )
+{
+  UINT32 RegValue;
+  ASSERT (DpmacId >= WRIOP_DPMAC1 && DpmacId < NUM_WRIOP_DPMACS);
+
+  RegValue = MmioRead32 ((UINTN)PcdGet64 (PcdMacDeviceDisableRegAddr));
+
+  return ((RegValue & BIT (DpmacId - 1)) == 0);
+}
+
+/**
+   Initializes a given WRIOP DPAA2 MAC. If the DPMAc is enabled,
+   it is added to the specified list of enabled DPMACs
+
+   @param[in] DpMacId                DPAA2 MAC Id
+   @param[in] PhyInterfaceType      Ethernet PHY interface type
+   @param[in] MdioBus               Pointer to the MDIO bus that connects the DMPAC to a PHY
+   @param[in] PhyAddress            PHY address on the MDIO bus
+   @param[in] EnabledDpmacList      Pointer to the head of the list of enabled DPMACs
+
+   @retval None
+
+ **/
+VOID
+WriopDpmacInit (
+  WRIOP_DPMAC_ID      DpmacId,
+  PHY_INTERFACE_TYPE  PhyInterfaceType,
+  DPAA2_PHY_MDIO_BUS  *MdioBus,
+  UINT8               PhyAddress,
+  PHY_MEDIA_TYPE      PhyMediaType,
+  LIST_ENTRY          *EnabledDpmacsList
+  )
+{
+  STATIC WRIOP_DPMAC *Dpmac;
+
+  ASSERT (DpmacId >= WRIOP_DPMAC1 && DpmacId < NUM_WRIOP_DPMACS);
+  ASSERT (PhyInterfaceType < NUM_PHY_INTERFACE_TYPES);
+
+  Dpmac = &gWriopDpmacs[DpmacId];
+  ASSERT (Dpmac->Id == DpmacId);
+  Dpmac->Enabled = IsDpmacEnabled (DpmacId);
+  Dpmac->Phy.PhyInterfaceType = PhyInterfaceType;
+  Dpmac->Phy.MdioBus = MdioBus;
+  Dpmac->Phy.PhyAddress = PhyAddress;
+  Dpmac->Phy.PhyMediaType = PhyMediaType;
+
+  DPAA_INFO_MSG (
+    "Dpmac %a  PHY type %a, %a\n",
+    gWriopDpmacStrings[DpmacId],
+    gPhyInterfaceTypeStrings[PhyInterfaceType],
+    Dpmac->Enabled ? "Enabled" : "Disabled");
+
+  if (Dpmac->Enabled) {
+    InitializeListHead (&Dpmac->ListNode);
+    InsertTailList (EnabledDpmacsList, &Dpmac->ListNode);
+  }
+}
+
+CONST
+CHAR8*
+PhyInterfaceTypeToString (
+  PHY_INTERFACE_TYPE PhyInterfaceType
+  )
+{
+  if (PhyInterfaceType < NUM_PHY_INTERFACE_TYPES) {
+    return gPhyInterfaceTypeStrings[PhyInterfaceType];
+  } else {
+    return "Unknown";
+  }
+}
