8bdbfc7928075816edb37ef2aeb4e84ef8a4f0c8
Pankaj Bansal <pankaj.bansal@nxp.com>
2018-10-04 13:07:14 +0530

    fdt_fixup: Add sysclk fixup
    Add sysclk fixup
    
    Signed-off-by: Pankaj Bansal <pankaj.bansal@nxp.com>

--- Silicon/NXP/Drivers/DtPlatformDxe/DtPlatformDxe.c
+++ Silicon/NXP/Drivers/DtPlatformDxe/DtPlatformDxe.c
@@ -19,10 +19,54 @@
 #include <Library/DevicePathLib.h>
 #include <Library/DxeServicesLib.h>
 #include <Library/MemoryAllocationLib.h>
+#include <Library/SocClockLib.h>
 #include <Library/UefiBootServicesTableLib.h>
 #include <Library/UefiDriverEntryPoint.h>
 
 #include "DtPlatformDxe.h"
+
+/**
+  The functions fixes the Input system clock frequency (SYSCLK) in device tree
+
+  for more details please refer https://www.kernel.org/doc/Documentation/devicetree/bindings/clock/qoriq-clock.txt
+
+  @param[in] Dtb        Device Tree to fix up.
+
+  @retval EFI_SUCCESS       system's SYSCLK value updated in sysclk node in device tree
+  @retval EFI_NOT_FOUND     Either could not get the valid SYSCLK value or
+                            the sysclk node not found in device tree
+  @retval EFI_DEVICE_ERROR  Failed to set values in device tree
+**/
+STATIC
+EFI_STATUS
+FdtSysClockFixup (
+  IN  VOID *Dtb
+  )
+{
+  UINT32  SysClk;
+  INTN    NodeOffset;
+  INTN    FdtStatus;
+
+  SysClk = SocGetClock (IP_SYSCLK, 0);
+  if (SysClk == 0) {
+    DEBUG ((DEBUG_ERROR, "Invalid System Clock\n"));
+    return EFI_NOT_FOUND;
+  }
+
+  NodeOffset = fdt_path_offset (Dtb, "/sysclk");
+  if (NodeOffset < 0) {
+    DEBUG ((DEBUG_ERROR, "No sysclk nodes found!!!\n"));
+    return EFI_NOT_FOUND;
+  }
+
+  FdtStatus = fdt_setprop_u32 (Dtb, NodeOffset, "clock-frequency", SysClk);
+  if (FdtStatus) {
+    DEBUG ((DEBUG_ERROR, "fdt_setprop/sysclk: Could not add property, %a!!\n", fdt_strerror (FdtStatus)));
+    return EFI_DEVICE_ERROR;
+  }
+
+  return EFI_SUCCESS;
+}
 
 /**
   The entry point for DtPlatformDxe driver.
@@ -83,6 +127,12 @@ DtPlatformDxeEntryPoint (
   // TODO: Verify the signed dtb image and then copy
   if (fdt_open_into (Dtb, Dtb, DtbSize)) {
     Status = EFI_BAD_BUFFER_SIZE;
+    goto FreeDtb;
+  }
+
+  // All Platforms need SYS clock frequency
+  Status = FdtSysClockFixup (Dtb);
+  if (EFI_ERROR (Status)) {
     goto FreeDtb;
   }
 
