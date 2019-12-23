e4b5a10944d84ef078d657757775ce21e70923c9
Pankaj Bansal <pankaj.bansal@nxp.com>
2019-12-23 14:52:42 +0530

    Move IORT strcture definitions to a header file
    We need IORT strcture definitions in PciHostBridgeLib so as to be
    able to do IORT fixups.
    
    Therefore, move IORT strcture definitions from IORT aslc to header
    file.
    
    Contributed-under: TianoCore Contribution Agreement 1.1
    Signed-off-by: Pankaj Bansal <pankaj.bansal@nxp.com>

---
+++ Silicon/NXP/Include/IndustryStandard/NxpIoRemappingTable.h
@@ -0,0 +1,60 @@
+/** @file
+*  I/O Remapping Table (Iort)
+*
+*  Copyright 2019 NXP
+*
+*  SPDX-License-Identifier: BSD-2-Clause
+*
+**/
+#ifndef __NXP_IO_REMAPPING_TABLE_H__
+#define __NXP_IO_REMAPPING_TABLE_H__
+
+#include <IndustryStandard/IoRemappingTable.h>
+
+#pragma pack(1)
+
+typedef struct
+{
+  EFI_ACPI_6_0_IO_REMAPPING_ITS_NODE          ItsNode;
+  UINT32                                      ItsIdentifiers[1];
+} NXP_EFI_ACPI_6_0_IO_REMAPPING_ITS_NODE;
+
+typedef struct
+{
+  EFI_ACPI_6_0_IO_REMAPPING_RC_NODE         PciRcNode;
+  //
+  // 32 mappings per PCIE controller
+  //
+  EFI_ACPI_6_0_IO_REMAPPING_ID_TABLE        PciRcIdMapping[32];
+} NXP_EFI_ACPI_6_0_IO_REMAPPING_RC_NODE;
+
+typedef struct
+{
+  EFI_ACPI_6_0_IO_REMAPPING_SMMU_NODE         SmmuNode;
+  EFI_ACPI_6_0_IO_REMAPPING_SMMU_INT          ContextInterrupt[64];
+  EFI_ACPI_6_0_IO_REMAPPING_SMMU_INT          PmuInterrupt[10];
+  //
+  // 32 mappings per PCIE controller + 1 mapping for MC bus
+  //
+  EFI_ACPI_6_0_IO_REMAPPING_ID_TABLE          SmmuID[65];
+} NXP_EFI_ACPI_6_0_IO_REMAPPING_SMMU_NODE;
+
+typedef struct
+{
+  EFI_ACPI_6_0_IO_REMAPPING_NAMED_COMP_NODE   NamedNode;
+  UINT8                                       ObjectName[20];
+  EFI_ACPI_6_0_IO_REMAPPING_ID_TABLE          NamedID[1];
+} NXP_EFI_ACPI_6_0_IO_REMAPPING_NAMED_COMP_NODE;
+
+typedef struct
+{
+  EFI_ACPI_6_0_IO_REMAPPING_TABLE                  Header;
+  NXP_EFI_ACPI_6_0_IO_REMAPPING_ITS_NODE           ItsNode;
+  NXP_EFI_ACPI_6_0_IO_REMAPPING_SMMU_NODE          SmmuNode;
+  NXP_EFI_ACPI_6_0_IO_REMAPPING_RC_NODE            PciRcNode[2];
+  NXP_EFI_ACPI_6_0_IO_REMAPPING_NAMED_COMP_NODE    NamedCompNode[9];
+} NXP_EFI_ACPI_6_0_IO_REMAPPING_TABLE;
+
+#pragma pack ()
+
+#endif
