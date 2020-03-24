05aaa96a8f274c41d4de160f3408dd046464634d
Pramod Kumar <pramod.kumar_1@nxp.com>
2020-03-24 14:58:13 +0530

    ls1046afrwy: Add usb support
    ls1046afrwy board support two usb ports.
    Add support to enable both usb.
    
    Signed-off-by: Pramod Kumar <pramod.kumar_1@nxp.com>

---
+++ Platform/NXP/LS1046aFrwyPkg/Library/PlatformLib/ArmPlatformInternalLib.h
@@ -0,0 +1,14 @@
+/** @file
+
+ Copyright 2020 NXP
+
+ SPDX-License-Identifier: BSD-2-Clause-Patent
+**/
+
+#ifndef FRWY_LS1046__BOARD_SPECIFIC_H__
+#define FRWY_LS1046__BOARD_SPECIFIC_H__
+
+
+#define USB2_MUX_SEL_GPIO    23
+
+#endif //__FRWY_LS1046__BOARD_SPECIFIC_H__
