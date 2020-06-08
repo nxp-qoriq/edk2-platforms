6a578f200fac5bae94486074546fe7adc63f53ef
Sahil Malhotra <sahil.malhotra@nxp.com>
2020-06-08 21:52:24 +0530

    Silicon/NXP/I2cLib: add function for writing on I2c Device
    There was no function in I2cLib for writing. So added function
    for same.
    Also changed I2cWrite function for continuos write.
    
    Signed-off-by: Sahil Malhotra <sahil.malhotra@nxp.com>

--- Silicon/NXP/Include/Library/I2cLib.h
+++ Silicon/NXP/Include/Library/I2cLib.h
@@ -117,4 +117,36 @@ I2cBusReadReg (
   IN  UINT32  RegValueNumBytes
   );
 
+/**
+  Write a register from I2c slave device. This API is wrapper around I2cBusXfer
+
+  @param[in]  Base                   Base Address of I2c controller's registers
+  @param[in]  SlaveAddress           Slave Address from which register value is
+                                     to be read
+  @param[in]  RegAddress             Register Address in Slave's memory map
+  @param[in]  RegAddressWidthInBytes Number of bytes in RegAddress to send to
+                                     I2c Slave for simple reads without any
+                                     register, make this value = 0
+                                     (RegAddress is don't care in that case)
+  @param[out] RegValue               Value to be read from I2c slave's regiser
+  @param[in]  RegValueNumBytes       Number of bytes to read from I2c slave
+                                     register
+
+  @return  EFI_SUCCESS       successfuly read the registers
+  @return  EFI_DEVICE_ERROR  There was an error while transferring data through
+                             I2c bus
+  @return  EFI_NO_RESPONSE   There was no Ack from i2c device
+  @return  EFI_TIMEOUT       I2c Bus is busy
+  @return  EFI_NOT_READY     I2c Bus Arbitration lost
+**/
+EFI_STATUS
+I2cBusWriteReg (
+  IN  UINTN   Base,
+  IN  UINT32  SlaveAddress,
+  IN  UINT64  RegAddress,
+  IN  UINT8   RegAddressWidthInBytes,
+  OUT UINT8   *RegValue,
+  IN  UINT32  RegValueNumBytes
+  );
+
 #endif // I2C_LIB_H__
