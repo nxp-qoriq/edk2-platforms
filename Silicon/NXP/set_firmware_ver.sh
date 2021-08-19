nxp-qoriq/edk2-platforms

commit 09e37c030109bc93e78168dc5a1d914e308a7e26

Platform/NXP : Add ConfigurationManagerDxe in fdf file
Summary -
commit 168d502a9d60ad5cf5c6927346ad6847fb6f99d7 has removed the
ConfigurationManagerDxe driver from firmware Volume.
Due to which ACPI/SMBIOS tables are not installed.
Above issue is resolved by this change and also removed the
set_firmware_ver.sh from Silicon/NXP

Signed-off-by: Vikas Singh <vikas.singh@puresoftware.com>

Silicon/NXP/set_firmware_ver.sh
@@ -1,35 +1,0 @@
-#!/bin/sh
-#
-# Copyright 2020 NXP
-#
-# SPDX-License-Identifier: BSD-2-Clause-Patent
-#
-# parse PACKAGES_PATH and set FIRMWARE_VER based on that
-
-get_git_version()
-{
-  command -v git>/dev/null 2>&1
-  if [ $? -eq 0 ] && [ -n "$1" ]
-  then
-    head_or_tag=`git -C $1 describe --tag --dirty --broken --always 2>/dev/null`
-    printf $head_or_tag
-  fi
-}
-
-FIRMWARE_VER=
-IFS=:
-
-for dir in $PACKAGES_PATH; do
-  if [ -n "$FIRMWARE_VER" ]; then
-    FIRMWARE_VER="$FIRMWARE_VER;$(basename $dir):$(get_git_version $dir)"
-  else
-    FIRMWARE_VER=$(basename $dir):$(get_git_version $dir)
-  fi
-done
-
-unset IFS
-
-echo "FIRMWARE_VER=$FIRMWARE_VER"
-export FIRMWARE_VER=$FIRMWARE_VER
-
-echo "build edk2 firmware with -D FIRMWARE_VER=\$FIRMWARE_VER"

Platform/NXP/LS1046aFrwyPkg/LS1046aFrwyPkg.fdf
@@ -105,6 +105,8 @@ READ_LOCK_STATUS   = TRUE
   !if $(DYNAMIC_ACPI_ENABLE) == FALSE
     INF RuleOverride=ACPITABLE Platform/NXP/LS1046aFrwyPkg/AcpiTables/AcpiTables.inf
   !else
+    INF Platform/NXP/ConfigurationManager/ConfigurationManagerDxe/ConfigurationManagerDxe.inf
+    INF DynamicTablesPkg/Drivers/DynamicTableManagerDxe/DynamicTableManagerDxe.inf
     INF DynamicTablesPkg/Drivers/DynamicTableFactoryDxe/DynamicTableFactoryDxe.inf
   !endif
   INF Platform/NXP/LS1046aFrwyPkg/AcpiTables/Icid.inf

Platform/NXP/LS1046aRdbPkg/LS1046aRdbPkg.fdf
@@ -106,6 +106,8 @@ READ_LOCK_STATUS   = TRUE
 !if $(DYNAMIC_ACPI_ENABLE) == FALSE
   INF RuleOverride=ACPITABLE Platform/NXP/LS1046aRdbPkg/AcpiTables/AcpiTables.inf
 !else
+  INF Platform/NXP/ConfigurationManager/ConfigurationManagerDxe/ConfigurationManagerDxe.inf
+  INF DynamicTablesPkg/Drivers/DynamicTableManagerDxe/DynamicTableManagerDxe.inf
   INF DynamicTablesPkg/Drivers/DynamicTableFactoryDxe/DynamicTableFactoryDxe.inf
 !endif
   INF Platform/NXP/LS1046aRdbPkg/AcpiTables/Icid.inf

Platform/NXP/LX2160aRdbPkg/LX2160aRdbPkg.fdf
@@ -105,6 +105,8 @@ READ_LOCK_STATUS   = TRUE
 !if $(DYNAMIC_ACPI_ENABLE) == FALSE
   INF RuleOverride=ACPITABLE Platform/NXP/LX2160aRdbPkg/AcpiTables/AcpiTables.inf
 !else
+  INF Platform/NXP/ConfigurationManager/ConfigurationManagerDxe/ConfigurationManagerDxe.inf
+  INF DynamicTablesPkg/Drivers/DynamicTableManagerDxe/DynamicTableManagerDxe.inf
   INF DynamicTablesPkg/Drivers/DynamicTableFactoryDxe/DynamicTableFactoryDxe.inf
 !endif
   INF Platform/NXP/LX2160aRdbPkg/AcpiTables/Icid.inf

commit abc064fee9542429edfc8166a7c91d03392fe5f3

Silicon/NXP: Fix firmware version information invalid in boot log
Add back shell script to collect version information before build.

Fixes: 09e37c03010 (“Platform/NXP : Add ConfigurationManagerDxe in fdf file”)
Signed-off-by: Ran Wang <ran.wang_1@nxp.com>

Silicon/NXP/set_firmware_ver.sh
@@ -1,0 +1,35 @@
+#!/bin/sh
+#
+# Copyright 2020 NXP
+#
+# SPDX-License-Identifier: BSD-2-Clause-Patent
+#
+# parse PACKAGES_PATH and set FIRMWARE_VER based on that
+
+get_git_version()
+{
+  command -v git>/dev/null 2>&1
+  if [ $? -eq 0 ] && [ -n "$1" ]
+  then
+    head_or_tag=`git -C $1 describe --tag --dirty --broken --always 2>/dev/null`
+    printf $head_or_tag
+  fi
+}
+
+FIRMWARE_VER=
+IFS=:
+
+for dir in $PACKAGES_PATH; do
+  if [ -n "$FIRMWARE_VER" ]; then
+    FIRMWARE_VER="$FIRMWARE_VER;$(basename $dir):$(get_git_version $dir)"
+  else
+    FIRMWARE_VER=$(basename $dir):$(get_git_version $dir)
+  fi
+done
+
+unset IFS
+
+echo "FIRMWARE_VER=$FIRMWARE_VER"
+export FIRMWARE_VER=$FIRMWARE_VER
+
+echo "build edk2 firmware with -D FIRMWARE_VER=\$FIRMWARE_VER"