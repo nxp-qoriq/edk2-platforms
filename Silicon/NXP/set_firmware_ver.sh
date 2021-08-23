nxp-qoriq/edk2-platforms

commit abc064fee9542429edfc8166a7c91d03392fe5f3

Silicon/NXP: Fix firmware version information invalid in boot log
Add back shell script to collect version information before build.

Fixes: 09e37c03010 (“Platform/NXP : Add ConfigurationManagerDxe in fdf file”)
Signed-off-by: Ran Wang <ran.wang_1@nxp.com>

Silicon/NXP/set_firmware_ver.sh
@@ -1,0 +1,35 @@
+#!/bin/sh
+#
+# Copyright 2020 NXP
+#
+# SPDX-License-Identifier: BSD-2-Clause-Patent
+#
+# parse PACKAGES_PATH and set FIRMWARE_VER based on that
+
+get_git_version()
+{
+  command -v git>/dev/null 2>&1
+  if [ $? -eq 0 ] && [ -n "$1" ]
+  then
+    head_or_tag=`git -C $1 describe --tag --dirty --broken --always 2>/dev/null`
+    printf $head_or_tag
+  fi
+}
+
+FIRMWARE_VER=
+IFS=:
+
+for dir in $PACKAGES_PATH; do
+  if [ -n "$FIRMWARE_VER" ]; then
+    FIRMWARE_VER="$FIRMWARE_VER;$(basename $dir):$(get_git_version $dir)"
+  else
+    FIRMWARE_VER=$(basename $dir):$(get_git_version $dir)
+  fi
+done
+
+unset IFS
+
+echo "FIRMWARE_VER=$FIRMWARE_VER"
+export FIRMWARE_VER=$FIRMWARE_VER
+
+echo "build edk2 firmware with -D FIRMWARE_VER=\$FIRMWARE_VER"

commit fb0f4baecbb35e17472502d590457b65b60e90b2

Silicon/NXP: Update copyright for set_firmware_ver.sh
Fixes: abc064fee954 (“Silicon/NXP: Fix firmware version information invalid in boot log”)
Signed-off-by: Ran Wang <ran.wang_1@nxp.com>

Silicon/NXP/set_firmware_ver.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# Copyright 2020 NXP
+# Copyright 2020-2021 NXP
 #
 # SPDX-License-Identifier: BSD-2-Clause-Patent
 #