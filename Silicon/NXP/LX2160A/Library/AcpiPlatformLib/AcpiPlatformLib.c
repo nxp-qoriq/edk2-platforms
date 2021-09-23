c438604fd289032a40939670ea1d28f4b357d7fb
Anda-Alexandra Dorneanu <anda-alexandra.dorneanu@nxp.com>
2021-09-23 09:50:54 +0300

    Platform: NXP: LX2160aRdbPkg: Add 10G PHYs addresses fixup in DSDT
    Update the MDIO ACPI table with the proper Aquantia 10G PHYs addresses.
    
    Signed-off-by: Anda-Alexandra Dorneanu <anda-alexandra.dorneanu@nxp.com>

--- Silicon/NXP/LX2160A/Library/AcpiPlatformLib/AcpiPlatformLib.c
+++ Silicon/NXP/LX2160A/Library/AcpiPlatformLib/AcpiPlatformLib.c
@@ -15,16 +15,233 @@
 #define AML_BUFFER_OP                0x11
 #define AML_RESOURCE_BUS             0x2
 
+#define PHY_ADDR1                    0x4
+#define PHY_ADDR2                    0x5
+#define PHY_ADDR1_REVC               0x0
+#define PHY_ADDR2_REVC               0x8
+
+static
+EFI_STATUS
+_FixPhyAddress(
+    IN EFI_ACPI_SDT_PROTOCOL  *AcpiTableProtocol,
+    IN EFI_ACPI_HANDLE        CurrentHandle
+    )
+{
+  EFI_STATUS          Status;
+  EFI_ACPI_DATA_TYPE  DataType;
+  CONST UINT8         *Data;
+  CONST VOID          *Buffer;
+  UINTN               DataSize;
+  UINT8               PhyAddr[1];
+
+  Status = AcpiTableProtocol->GetOption(CurrentHandle, 1, &DataType, &Buffer, &DataSize);
+  if (EFI_ERROR(Status)) {
+    return Status;
+  }
+
+  Data = Buffer;
+  if (Data[0] != PHY_ADDR1 && Data[0] != PHY_ADDR2) {
+    return EFI_SUCCESS;
+  }
+
+  PhyAddr[0] = (Data[0] == PHY_ADDR1) ? PHY_ADDR1_REVC : PHY_ADDR2_REVC;
+  Status = AcpiTableProtocol->SetOption(CurrentHandle, 1, (VOID*)&PhyAddr[0], sizeof(UINT8));
+
+  return Status;
+}
+
+static
+EFI_STATUS
+ProcessDSDTDevice (
+    EFI_ACPI_SDT_PROTOCOL *AcpiTableProtocol,
+    EFI_ACPI_HANDLE ChildHandle
+    )
+{
+  EFI_STATUS          Status;
+  EFI_ACPI_DATA_TYPE  DataType;
+  CONST UINT8         *Data;
+  CONST VOID          *Buffer;
+  UINTN               DataSize;
+  EFI_ACPI_HANDLE     DevHandle;
+
+  DBG ("[%a:%d] - ChildHandle=%p\n", __FUNCTION__, __LINE__, ChildHandle);
+  Status = AcpiTableProtocol->GetOption(ChildHandle, 0, &DataType, &Buffer, &DataSize);
+  if (EFI_ERROR(Status)) {
+    return EFI_SUCCESS;
+  }
+
+  Data = Buffer;
+  //
+  // Skip all non-device type
+  //
+  if (DataSize != 2 || Data[0] != AML_EXT_OP || Data[1] != AML_EXT_DEVICE_OP) {
+    return EFI_SUCCESS;
+  }
+
+  Status = AcpiTableProtocol->GetOption(ChildHandle, 1, &DataType, &Buffer, &DataSize);
+  if (EFI_ERROR(Status)) {
+    return EFI_SUCCESS;
+  }
+
+  Data = Buffer;
+  if (AsciiStrnCmp((CHAR8 *)Data, "PHY", 3) == 0) {
+    //
+    // Walk the PHY device type node
+    //
+    for (DevHandle = NULL; ; ) {
+      Status = AcpiTableProtocol->GetChild(ChildHandle, &DevHandle);
+      if (EFI_ERROR(Status) || DevHandle == NULL) {
+        break;
+      }
+
+      //
+      // Search for _ADR
+      //
+      Status = AcpiTableProtocol->GetOption(DevHandle, 0, &DataType, &Buffer, &DataSize);
+      if (EFI_ERROR(Status)) {
+        break;
+      }
+
+      Data = Buffer;      
+      if (DataSize == 1 && Data[0] == AML_NAME_OP) {
+        Status = AcpiTableProtocol->GetOption(DevHandle, 1, &DataType, &Buffer, &DataSize);
+        if (EFI_ERROR(Status)) {
+          break;
+        }
+
+        Data = Buffer;
+        if (DataType == EFI_ACPI_DATA_TYPE_NAME_STRING) {
+          if (AsciiStrnCmp((CHAR8 *) Data, "_ADR", 4) == 0) {
+            EFI_ACPI_HANDLE ValueHandle;
+
+            Status = AcpiTableProtocol->GetOption(DevHandle, 2, &DataType, &Buffer, &DataSize);
+            if (EFI_ERROR(Status)) {
+              break;
+            }
+
+            if (DataType != EFI_ACPI_DATA_TYPE_CHILD) {
+              continue;
+            }
+
+            AcpiTableProtocol->Open((VOID *) Buffer, &ValueHandle);
+            Status = _FixPhyAddress(AcpiTableProtocol, ValueHandle);
+            if (EFI_ERROR(Status)) {
+              AcpiTableProtocol->Close(ValueHandle);
+              break;
+            }
+            AcpiTableProtocol->Close(ValueHandle);
+          }
+        }
+      }       
+    }
+  }
+
+  return EFI_SUCCESS;
+}
+
+static
+BOOLEAN
+IsSbScope (
+    EFI_ACPI_SDT_PROTOCOL   *AcpiTableProtocol,
+    EFI_ACPI_HANDLE         ChildHandle
+    )
+{
+  EFI_STATUS          Status;
+  EFI_ACPI_DATA_TYPE  DataType;
+  CONST UINT8         *Data;
+  CONST VOID          *Buffer;
+  UINTN               DataSize;
+
+  DBG ("[%a:%d] - ChildHandle=%p\n", __FUNCTION__, __LINE__, ChildHandle);
+  Status = AcpiTableProtocol->GetOption (ChildHandle, 0, &DataType, &Buffer, &DataSize);
+  if (EFI_ERROR(Status)) {
+    return FALSE;
+  }
+
+  Data = Buffer;
+  if (DataSize != 1 || Data[0] != AML_SCOPE_OP) {
+    return FALSE;
+  }
+
+  return TRUE;
+}
+
+static
+EFI_STATUS
+ProcessDSDTChild (
+    EFI_ACPI_SDT_PROTOCOL *AcpiTableProtocol,
+    EFI_ACPI_HANDLE ChildHandle
+    )
+{
+  EFI_STATUS          Status;
+  EFI_ACPI_HANDLE     DevHandle;
+
+  DBG ("[%a:%d] - ChildHandle=%p\n", __FUNCTION__, __LINE__, ChildHandle);
+  // Check Scope(_SB) at first
+  if (!(Status = IsSbScope (AcpiTableProtocol, ChildHandle))) {
+    DBG ("[%a:%d] - IsSbScope=%r\n", __FUNCTION__, __LINE__, Status);
+    return ProcessDSDTDevice (AcpiTableProtocol, ChildHandle);
+  }
+
+  for (DevHandle = NULL; ; ) {
+    Status = AcpiTableProtocol->GetChild (ChildHandle, &DevHandle);
+    if (EFI_ERROR(Status) || DevHandle == NULL) {
+      break;
+    }
+
+    ProcessDSDTDevice (AcpiTableProtocol, DevHandle);
+  }
+
+  return EFI_SUCCESS;
+}
+
 EFI_STATUS
 PlatformProcessDSDT (
     EFI_ACPI_SDT_PROTOCOL *AcpiTableProtocol,
     EFI_ACPI_HANDLE TableHandle
-    ) { return EFI_SUCCESS; }
+    ) 
+{
+  
+  EFI_STATUS              Status;
+  EFI_ACPI_HANDLE         ChildHandle;
+  //
+  // Parse table for device type
+  for (ChildHandle = NULL; ; ) {
+    Status = AcpiTableProtocol->GetChild(TableHandle, &ChildHandle);
+    if (EFI_ERROR(Status)) {
+      break;
+    }
+    if (ChildHandle == NULL) {
+      break;
+    }
+
+    ProcessDSDTChild(AcpiTableProtocol, ChildHandle);
+  }
+ 
+  return EFI_SUCCESS; 
+}
 
 void
 PlatformAcpiCheckSum (
     IN OUT  EFI_ACPI_SDT_HEADER *Table
-    ) { /* Do Nothing */ }
+    )
+{
+  UINTN ChecksumOffset;
+  UINT8 *Buffer;
+
+  ChecksumOffset = OFFSET_OF (EFI_ACPI_DESCRIPTION_HEADER, Checksum);
+  Buffer = (UINT8 *)Table;
+
+  //
+  // set checksum to 0 first
+  //
+  Buffer[ChecksumOffset] = 0;
+
+  //
+  // Update checksum value
+  //
+  Buffer[ChecksumOffset] = CalculateCheckSum8 (Buffer, Table->Length);
+}
 
 /**
   Fixup the DSDT Table based on running SOC's properties.
